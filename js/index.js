784b505fd1fbffa8cecaead64a96f1bb7123e73a
Andrew <Kusnir8655@gmil.com>
2023-01-11 20:36:19 +0100

    Add section aboutus-hero

--- js/index.js
+++ js/index.js
@@ -1,12 +1,21 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
 
-const nav = document.querySelector('#navbar')
-function addBackground() {
-   if (window.scrollY >= 100) {
-    nav.classList.add('shadow-bg')
-   } else {
-    nav.classList.remove('shadow-bg')
-   }
-}
-window.addEventListener('scroll', addBackground)
+    const nav = document.querySelector('#navbar')
+    function addBackground() {
+        if (window.scrollY >= 100) {
+            nav.classList.add('shadow-bg')
+        } else {
+            nav.classList.remove('shadow-bg')
+        }
+    }
+    window.addEventListener('scroll', addBackground)
 })
+
+const collapse = () => {
+    const burgerBar = document.querySelector('.collapse')
+    if (burgerBar.classList.contains('show')) {
+        burgerBar.classList.remove('show')
+    }
+}
+document.addEventListener('click', collapse);
+
